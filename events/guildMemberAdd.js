995e88bc612514d8a2a30533a89a5685911321ed
VixeyTheVixen <61295462+VixeyTheVixen@users.noreply.github.com>
2020-05-20 07:01:43 -0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,34 @@
+const moment = require('moment')
+const Discord = require('discord.js')
+
+/**
+ * O evento guildMemberAdd é emitido após um membro entrar (ser adicionado em uma guild).
+ */
+
+module.exports = async (client, member) => {
+  // Verificações anti-selfbot de divulgação já que estamos tendo problemas com isso.
+  const daysSinceCreation = moment().diff(moment(member.user.createdAt), 'days')
+  const isDefaultAvatar = member.user.displayAvatarURL.startsWith('https://discordapp.com/')
+  const domaincount = member.user.username.match(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/)
+  if (domaincount > 0 && (isDefaultAvatar || daysSinceCreation < 3)) return (() => { member.send('Olá! você foi kickado automaticamente por suspeita de divulgação em nosso servidor. Contas com menos de 3 dias no discord não podem ter domínios (exemplo twitter.com)').catch(); member.kick('Autokick: Selfbots não são bem vindos').catch() })()
+
+  const message = new Discord.RichEmbed()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setColor('RANDOM')
+    .setAuthor('👋 Bem-vindo(a) ao DGP Mundo!')
+    .setTitle('Aproveite sua conversa em <#440913811994705921>!')
+    .setDescription(`${member}, vá em <#709479485858250815> e leia as regras.`)
+    .setFooter('2020 - DGP Mundo')
+    .setTimestamp()
+
+  const join = new Discord.RichEmbed()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setColor('RANDOM')
+    .setAuthor('✨ Um novo membro entrou no servidor!')
+    .setDescription(`${member} acabou de entrar.`)
+    .setFooter('2020 - DGP Mundo')
+    .setTimestamp()
+
+  member.guild.channels.get(process.env.JOINCHANNEL).send(join).catch()
+  member.guild.channels.get(process.env.GREETCHANNEL).send(message).catch()
+}
